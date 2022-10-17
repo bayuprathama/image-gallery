8cd865ccc43d1233563177ce8f034ea7549cbbac
Bayu Prathama <dev.bayuprathama@gmail.com>
2022-10-17 16:25:06 +0800

    fix revalidate

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,16 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default async function handler(req, res) {
-  // check for secret token
+  // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
-    return res.status(401).json({ message: 'Invalid Token' });
+    return res.status(401).json({ message: 'Invalid token' });
   }
 
   try {
-    // regenerate index route '/' when revalidated
-    await res.unstable_revalidate('/');
-    return res.json({ revalidate: true });
+    // this should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    await res.revalidate('/');
+    return res.json({ revalidated: true });
   } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
     return res.status(500).send('Error revalidating');
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import { useState } from 'react';
 import { cn } from '../config/cn';
 
 export default function Gallery({ images }) {
-  console.log(images);
   return (
     <div className="max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg-px-8">
       <Head>
