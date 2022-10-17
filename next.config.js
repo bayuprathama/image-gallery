ea5ff5013ccfe89e440dd84c014c17e9e7dd7e00
Bayu Prathama <dev.bayuprathama@gmail.com>
2022-10-17 15:51:41 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['bit.ly', 'scontent-sin6-2.cdninstagram.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
